c57e0081e72088d578713f64dd66f274200e9671
tabbakha, ahmad <tabba.ahmad@gmail.com>
2018-07-30 21:18:13 -0500

    remove comments

--- hello.py
+++ hello.py
@@ -12,9 +12,9 @@ print s , f , i
 
 str = 'Hello World!'
 
-print str          # Prints complete string
-print str[0]       # Prints first character of the string
-print str[2:5]     # Prints characters starting from 3rd to 5th
-print str[2:]      # Prints string starting from 3rd character
-print str * 2      # Prints string two times
-print str + "TEST" # Prints concatenated string
+print str
+print str[0]
+print str[2:5]
+print str[2:]
+print str * 2
+print str + "TEST"
