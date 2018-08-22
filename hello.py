5dac24ed451d6d3fbced7ab88f947548c8b90799
Tabbakha, Ahmad <tabba.ahmad@toyotaconnected.com>
2018-08-22 11:52:46 -0500

    adapt hello.py to python3

--- hello.py
+++ hello.py
@@ -1,29 +1,29 @@
 # Comment
 # this file has a ton of random code
-print "Hello World!"
+print ("Hello World!")
 print('Hello world!')
 
-print raw_input("\nEnter a value to print: ")
+print (input("\nEnter a value to print: "))
 
 s = "string"
 f = 100.0
 i = 100
 
-print s , f , i
+print (s , f , i)
 
 str = 'Hello World!'
 
-print str
-print str[0]
-print str[2:5]
-print str[2:]
-print str * 2
-print str + "TEST"
+print (str)
+print (str[0])
+print (str[2:5])
+print (str[2:])
+print (str * 2)
+print (str + "TEST")
 
-print 5/2
-print 5.0/2.0
+print (5/2)
+print (5.0/2.0)
 
-print "{} is a {}".format("This", "placeholder")
-print "{0} can be {1}".format("strings", "formatted")
+print ("{} is a {}".format("This", "placeholder"))
+print ("{0} can be {1}".format("strings", "formatted"))
 # You can use keywords if you don't want to count.
-print x"{name} wants to eat {food}".format(name="Ahmad", food="Ice Cream")
+print ("{name} wants to eat {food}".format(name="Ahmad", food="Ice Cream"))
